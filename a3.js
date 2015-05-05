5470da16e281d35270ed22b8a4dd03ddf8dd0fdf
Hannah Yoo <hyoo2@uw.edu>
2015-05-05 00:46:08 -0700

    Fixed commas in year & added titles to graph/axes

--- a3.js
+++ a3.js
@@ -1,17 +1,10 @@
-var margin = {top: 20, right: 80, bottom: 30, left: 50},
+var margin = {top: 30, right: 80, bottom: 30, left: 60},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 
-
 var color = d3.scale.category10();
 
-
-
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d) { return x(d.Year); })
-    .y(function(d) { return y(d.Count); });
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -20,6 +13,8 @@ var svg = d3.select("body").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("names.csv", function(error, data) {
+    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "Year"; }));
+
     data.forEach(function(d) {
         Year = +d.Year;
         Name = d.Name;
@@ -32,13 +27,18 @@ d3.csv("names.csv", function(error, data) {
         .range([0, width]);
 
     var y = d3.scale.linear()
-        .domain([0, 35000])
         .range([height, 0]);
+
+    var line = d3.svg.line()
+        .interpolate("basis")
+        .x(function(d) { return x(d.Year); })
+        .y(function(d) { return y(d.Count); });
 
 
     var xAxis = d3.svg.axis()
         .scale(x)
-        .orient("bottom");
+        .orient("bottom")
+        .tickFormat(d3.format("d"));
 
     var yAxis = d3.svg.axis()
         .scale(y)
@@ -48,7 +48,7 @@ d3.csv("names.csv", function(error, data) {
         return {
             name: name,
             values: data.map(function(d) {
-                return {Year: +[d.Year], Name: d.Name, Gender: d.Gender,
+                return {Year: [d.Year], Name: d.Name, Gender: d.Gender,
                     Count: +[d.Count], Ranking: +[d.Ranking]};
             })
         };
@@ -56,38 +56,59 @@ d3.csv("names.csv", function(error, data) {
 
     x.domain(d3.extent(data, function(d) { return d.Year; }));
 
+    y.domain([
+        d3.min(names, function(c) { return d3.min(c.values, function(v) { return v.Count; }); }),
+        d3.max(names, function(c) { return d3.max(c.values, function(v) { return v.Count; }); })
+    ]);
+
+
 
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(xAxis)
+        // x-axis title
+        .append("text")
+        .attr("x", (width / 2))
+        .attr("y", margin.bottom)
+        .attr("dx", ".71em")
+        .style("text-anchor", "middle")
+        .text("Year");
 
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
+        // y-axis title
         .append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 35000)
+        .attr("x", -(height / 2))
+        .attr("y", -margin.left)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Count");
+        .text("Count of Names");
 
+    // title
+    svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "bold")
+        .text("Top Popular Names");
 
     var name = svg.selectAll(".name")
-        .data(name)
+        .data(names)
         .enter().append("g")
         .attr("class", "name");
 
     name.append("path")
         .attr("class", "line")
-        .attr("d", function(d) { return line(data); })
-        //.style("stroke", function(d) { return color(d.Name); });
+        .attr("d", function(d) { return line(d.values); })
+        .style("stroke", function(d) { return color(d.Name); });
 
-
-   name.append("text")
-       .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-       .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.count) + ")"; })
-       .attr("x", 2020)
-       .attr("dy", ".35em")
-       .text(function(d) { return d.name; });
+    name.append("text")
+        .datum(function(d) { return {name: d.Name, value: d.values[d.values.length - 1]}; })
+        .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.count) + ")"; })
+        .attr("dy", ".35em")
+        .text(function(d) { return d.Name; });
 });
