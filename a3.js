a4daacd9c7c7d90bf09fbaf8576a095d9542ecf3
Hannah Yoo <hyoo2@uw.edu>
2015-05-06 23:19:01 -0700

    attempted drop downs

--- a3.js
+++ a3.js
@@ -1,39 +1,139 @@
-// sets dimensions of canvas of graph
 var margin = {top: 30, right: 80, bottom: 30, left: 60},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-// sets ranges
-var x = d3.scale.linear()
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
 var color = d3.scale.category10();
 
-// gets data
-d3.csv("names2.csv", function(error, data) {
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var myData = [];
+
+// HELP FROM HERE!!!!
+// filter data using drop down
+var dataSet = myData;
+
+var myType = "all";
+var patt = new RegExp("all");
+
+function filterType(myType) {
+    myType = this.myType;
+    var res = patt.test(myType);
+    if (res) {
+        var toVis = dataSet;
+    }
+    else {
+        var toVis = dataSet.filter(function(d, i) {
+            return d["type"] == myType;
+        });
+    }
+    drawVis(toVis);
+}
+
+function drawVis(myData) {
+
+}
+
+// TO HERE!!!!!
+
+var nameArr = [];
+
+var countYearName = [];
+
+d3.csv("names.csv", function(error, data) {
+
     color.domain(d3.keys(data[0]).filter(function(key) { return key !== "Year"; }));
 
     data.forEach(function(d) {
-         Year = +d.Year;
-         Name = d.Name;
-         Gender = d.Gender;
-         Count = +d.Count;
-         Ranking = +d.Ranking;
+        //Formats each year object
+        Year = +d.Year;
+        Name = d.Name;
+        Gender = d.Gender;
+        Count = +d.Count;
+        Ranking = +d.Ranking;
     });
 
-    var names = color.domain().map(function(name) {
-        return {
-            name: name,
-            values: data.map(function(d) {
-                return {Year: d.Year, Count: +d[name]};
-            })
-        };
+/*    d3.selectAll(".box").on('click', function() {
+        var value = this.value,
+            display = this.checked ? "inline" : "none";
+
+        svg.selectAll(".symbol")
+            .filter(function(d) { return d.properties.type === type; })
+            .attr("display", display);
+    });*/
+
+/*    function checkGender() {
+        var mChecked = document.getElementById("male").checked;
+        var fChecked = document.getElementById("female").checked;
+
+        if (mChecked && fChecked) {
+            names.filterAll();
+        }
+        else if (mChecked && !fChecked) {
+            names.filter('mChecked');
+        }
+        else if (!mChecked && fChecked) {
+            names.filter('fChecked');
+        }
+        else {
+            return;
+        }
+    }*/
+
+/*    //checkboxes data
+    var checkbox = svg.append("path")
+        .attr("class", "line")
+        .datum(function(d) { return data })
+        .attr("d", "path");
+
+    //checkboxes select
+    d3.selectAll(".box").on("change", function() {
+        var type = this.type,
+            display = this.checked ? "inline" : "none";
+
+        svg.selectAll(".symbol")
+            .filter(function(d) { return d.properties.type === type; })
+            .attr("display", display);
+    });*/
+
+    //This creates an array per name and sets the value of that array to the array of year objects for that name
+    data.forEach(function(d) {
+        countYearName.push({Year: +d.Year, Name: d.Name, Count: +d.Count});
+
+        var foundAtIndex = -1;
+
+        for (var i=0; i<=nameArr.length; i++) {
+            //console.log("comparing "+d.Name+" to "+nameArr[i].Name);
+            if(nameArr[i] && nameArr[i][0].Name){
+                if (d.Name == nameArr[i][0].Name){
+                    foundAtIndex = i;
+                }
+            }
+        }
+
+        if (foundAtIndex > -1) {
+            nameArr[foundAtIndex].unshift(d);
+        } else {
+            nameArr.push([d]);
+        }
     });
 
-    // defines axes
+    //console.log(countYearName);
+
+    var x = d3.scale.linear()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var line = d3.svg.line()
+        .interpolate("basis")
+        .x(function(d) { return (x(d.Year) + 60); })
+        .y(function(d) { return y(d.Count); });
+
     var xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom")
@@ -43,20 +143,16 @@ d3.csv("names2.csv", function(error, data) {
         .scale(y)
         .orient("left");
 
-    // defines line
-    var line = d3.svg.line()
-        .interpolate("basis")
-        .x(function(d) { return x(d.Year); })
-        .y(function(d) { return y(d.Count); });
+    var names = color.domain().map(function(name) {
+        return {
+            name: name,
+            values: data.map(function(d) {
+                return {Year: [d.Year], Name: d.Name, Gender: d.Gender,
+                    Count: +[d.Count], Ranking: +[d.Ranking]};
+            })
+        };
+    });
 
-    // adds svg canvas
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // scales range of data
     x.domain(d3.extent(data, function(d) { return d.Year; }));
 
     y.domain([
@@ -64,12 +160,11 @@ d3.csv("names2.csv", function(error, data) {
         d3.max(names, function(c) { return d3.max(c.values, function(v) { return v.Count; }); })
     ]);
 
-    // adds value line path
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
-        // adds x-axis title
+        // x-axis title
         .append("text")
         .attr("x", (width / 2))
         .attr("y", margin.bottom)
@@ -80,7 +175,7 @@ d3.csv("names2.csv", function(error, data) {
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
-        // adds y-axis title
+        // y-axis title
         .append("text")
         .attr("transform", "rotate(-90)")
         .attr("x", -(height / 2))
@@ -89,49 +184,43 @@ d3.csv("names2.csv", function(error, data) {
         .style("text-anchor", "end")
         .text("Count of Names");
 
-    // add title of graph
+    // title
     svg.append("text")
         .attr("x", (width / 2))
         .attr("y", 0 - (margin.top / 2))
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
         .style("text-decoration", "bold")
-        .text("Top Popular Names");
+        .text("Top 20 Popular Names 1993 - 2013");
 
-    var name = svg.selectAll(".name")
-        .data(names)
+    var name = d3.select("svg")
+        .selectAll(".name")
+        .data(nameArr)
         .enter().append("g")
-        .attr("class", "name");
-
-    name.append("path")
+        .attr("class", "name")
+        .append("path")
         .attr("class", "line")
-        .attr("d", function(d) { return line(d.values); })
-        .style("stroke", function(d) { return color(d.Name); });
+        .attr("d", function(d, i) { return line(d); })
+        .style("stroke", function(d) { return color(d[0].Name);
+        })
+        .append("svg:title")
+        .text(function(d, i) {
+            return "name: " + d[0].Name + " year: " + d[0].Year + " gender: " + d[0].Gender;
+        })
 
-    /*
-     name.append("text")
-     .datum(function(d) { return {name: d.Name, value: d.values[d.values.length - 1]}; })
-     .attr("transform", function(d) { return "translate(" + x(d.value.Year) + "," + y(d.value.Count) + ")"; })
-     .attr("dy", ".35em")
-     .text(function(d) { return d.Name; })
-     */
+    svg.selectAll(".name")
+        .on("mouseover", function(d){
+            d3.selectAll(d.name).style("visibility", "visible")
+        });
 
+    var display;
 
-    /*
-     // checkboxes data
-     var checkbox = svg.append("path")
-     .attr("class", "line")
-     .datum(function(d) { return data })
-     .attr("d", path);
-
-     // checkboxes select
-     d3.selectAll(".box").on("change", function() {
-     var type = this.type,
-     display = this.checked ? "inline" : "none";
-
-     svg.selectAll(".symbol")
-     .filter(function(d) { return d.properties.type === type; })
-     .attr("display", display);
-     });
-     */
+    d3.selectAll(".filter_button").on("change", function () {
+        var selected = this.value,
+            display = this.checked ? "inline" : "none";
+        alert(selected);
+        svg.selectAll(".name")
+            .filter(function(d) { return d[0].Gender == selected; })
+            .attr("display", display);
+    });
 });
